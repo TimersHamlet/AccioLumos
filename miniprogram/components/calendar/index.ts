7b5702d9a8e57ba922bd7144d4d012fb1a198314
何佳乐(20017864) <20017864@suning.com>
2021-05-28 10:57:50 +0800

    Update:日历组件化

---
+++ miniprogram/components/calendar/index.ts
@@ -0,0 +1,27 @@
+// components/calendar/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {},
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    weekData: [
+      { label: '一' },
+      { label: '二' },
+      { label: '三' },
+      { label: '四' },
+      { label: '五' },
+      { label: '六' },
+      { label: '日' }
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {}
+});
